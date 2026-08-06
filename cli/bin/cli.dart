a005167a9a372a0e1bbf6245f5d45b0ac8b10137
mayank <knowledgesmart24@gmail.com>
2026-08-06 22:34:06 +0530

    Add command runner package with initial implementation and dependencies

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,5 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'package:command_runner/command_runner.dart';
+
 const version = '0.0.1';
 
 
---
+++ command_runner/lib/command_runner.dart
@@ -0,0 +1,4 @@
+library;
+
+export 'src/command_runner_base.dart';
+// TODO: Export any other libraries intended for clients of this package.
